893c720b45b874d43aed7278ad649d9e4268d1fc
mukhsini <sammccain95@gmail.com>
2018-07-31 13:52:01 +0300

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 app.use(helmet());
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb://shukuru:mukhsini01@ds243441.mlab.com:43441/salome';
+var mongoDB ='mongodb://shukuru:mukhsini01@ds243441.mlab.com:43441/salome';
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
